95cd7325efbb92a13dfc65a55d586b251ea930e8
sagarlama0102 <sagarlama0102@gmail.com>
2026-01-14 19:13:54 +0545

    created user session and storage service

---
+++ lib/core/services/storage/user_session_service.dart
@@ -0,0 +1,90 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError(
+    "Shared preferences lai handle main.dart ma initilize garincha",
+  );
+});
+
+//provider
+final userSessionServiceProvider = Provider<UserSessionService>((ref) {
+  return UserSessionService(prefs: ref.read(sharedPreferencesProvider));
+});
+
+class UserSessionService {
+  final SharedPreferences _prefs;
+
+  UserSessionService({required SharedPreferences prefs}) : _prefs = prefs;
+
+  //keys for storing data
+  static const String _keyIsLoggedIn = "is_logged_in";
+  static const String _keyUserId = "user_id";
+  static const String _keyUserEmail = "user_email";
+  static const String _keyUsername = "username";
+  static const String _keyUserFirstName = "user_first_name";
+  static const String _keyUserLastName = "user_last_name";
+  static const String _keyUserPhoneNumber = "user_phone_number";
+
+  //store user session data
+  Future<void> saveUserSession({
+    required String userId,
+    required String email,
+    required String username,
+    required String firstName,
+    required String lastName,
+    required String? phoneNumber,
+
+  })async {
+    await _prefs.setBool(_keyIsLoggedIn, true);
+    await _prefs.setString(_keyUserId, userId);
+    await _prefs.setString(_keyUserEmail, email);
+    await _prefs.setString(_keyUsername, username);
+    await _prefs.setString(_keyUserFirstName, firstName);
+    await _prefs.setString(_keyUserLastName, lastName);
+
+    if (phoneNumber != null) {
+      await _prefs.setString(_keyUserPhoneNumber, phoneNumber);
+    }
+
+  }
+  // clear user session data
+  Future<void> clearUserSession() async {
+    await _prefs.remove(_keyUserId);
+    await _prefs.remove(_keyIsLoggedIn);
+    await _prefs.remove(_keyUserFirstName);
+    await _prefs.remove(_keyUserLastName);
+    await _prefs.remove(_keyUserPhoneNumber);
+    await _prefs.remove(_keyUsername);
+    await _prefs.remove(_keyUserEmail);
+  }
+  bool isLoggedIn() {
+    return _prefs.getBool(_keyIsLoggedIn) ?? false;
+  }
+
+  String? getUserId() {
+    return _prefs.getString(_keyUserId);
+  }
+
+  String? getUserEmail() {
+    return _prefs.getString(_keyUserEmail);
+  }
+
+  String? getUsername() {
+    return _prefs.getString(_keyUsername);
+  }
+
+  String? getUserFirstName() {
+    return _prefs.getString(_keyUserFirstName);
+  }
+
+  String? getUserLastName() {
+    return _prefs.getString(_keyUserLastName);
+  }
+
+  String? getUserPhoneNumber() {
+    return _prefs.getString(_keyUserPhoneNumber);
+  }
+
+
+}
