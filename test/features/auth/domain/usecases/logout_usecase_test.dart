5b6ac918c570fcfd980685a7f6811c4db7ecc574
sagarlama0102 <sagarlama0102@gmail.com>
2026-01-28 16:04:07 +0545

    login usecase test implemented and success return

---
+++ test/features/auth/domain/usecases/logout_usecase_test.dart
@@ -0,0 +1,12 @@
+// import 'package:flutter_test/flutter_test.dart';
+
+// void main() {
+//   late LogoutUsecase usecase;
+//   late MockAuthRepository mockRepository;
+
+//   setUp(() {
+//     mockRepository = MockAuthRepository();
+//     usecase = LogoutUsecase(authRepository: mockRepository);
+//   });
+//   });
+// }
